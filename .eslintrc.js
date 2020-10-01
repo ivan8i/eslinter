6ab11600138924547f0f0878af58769dae9a49dc
Ivan <ivan8i@users.noreply.github.com>
2020-10-01 10:59:54 +0800

    Bumped version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,32 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  "plugins": [
-    "react",
-    "react-hooks"
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended"
-  ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 2018
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
   },
-  "settings": {
-    "react": {
-      "version": "detect"   
-    }
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
-  "rules": {
-    "linebreak-style": ["error", "unix"],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  }
-};
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
